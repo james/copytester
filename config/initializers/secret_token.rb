f32de89ddf69eaef9d813865ed4c8dc304f42e01
James Darling <james@abscond.org>
2014-02-04 20:44:54 +0000

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Copytester::Application.config.secret_key_base = '080884fada02f793cae440d10518009a32e80fb44a12b284ca5f0a3bf80eea1719faefea0640f05bf3db7accb8009895264ab89292e90c94eaf6c9923f21394b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Copytester::Application.config.session_store :cookie_store, key: '_copytester_session'
