3f235d1853db3aaa0a4b29f076afd89ec4fa12ca
James Darling <james@abscond.org>
2014-02-06 19:09:06 +0000

    Add editing of copy samples

--- app/controllers/admin/copy_samples_controller.rb
+++ app/controllers/admin/copy_samples_controller.rb
@@ -5,4 +5,15 @@ class Admin::CopySamplesController < ApplicationController
     @copy_sample.save!
     redirect_to edit_admin_copy_test_path(@copy_test)
   end
+  def edit
+    @copy_test = CopyTest.find(params[:copy_test_id])
+    @copy_sample = @copy_test.copy_samples.find(params[:id])
+  end
+  def update
+    @copy_test = CopyTest.find(params[:copy_test_id])
+    @copy_sample = @copy_test.copy_samples.find(params[:id])
+    @copy_sample.update_attributes(params[:copy_sample].permit(:name, :copy))
+    @copy_sample.save!
+    redirect_to edit_admin_copy_test_path(@copy_test)
+  end
 end
