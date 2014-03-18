2ec9ea195fc5e85f3340871506f5372b2b132a01
James Darling <james@abscond.org>
2014-03-18 12:11:42 +0000

    randomise order of trust rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def trust_ratings
+    r = [
+      ["Strongly Disagree", 1],
+      ["Disagree", 2],
+      ["Neutral", 3],
+      ["Agree", 4],
+      ["Strongly Agree", 5]
+    ]
+    rand > 0.5 ? r : r.reverse
+  end
 end
